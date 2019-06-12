2dcb672d8e5091272bafc984c8ab9f8ad0df5415
Vladislav Volkov <wwwvladislav@gmail.com>
2019-06-12 23:13:29 +0300

    LMDB API

--- mdv_core/storage/mdv_storage.c
+++ mdv_core/storage/mdv_storage.c
@@ -1,5 +1,183 @@
 #include "mdv_storage.h"
 #include <mdv_log.h>
 #include <mdv_filesystem.h>
+#include <mdv_alloc.h>
+#include <stdatomic.h>
+#include <lmdb.h>
+
+
+struct mdv_storage
+{
+    atomic_int  ref_counter;
+    MDB_env    *env;
+};
+
+
+mdv_storage * mdv_storage_open(char const *path, uint32_t dbs_num, uint32_t flags)
+{
+    if (!path)
+    {
+        MDV_LOGE("DB path is empty");
+        return 0;
+    }
+
+    if (!mdv_mkdir(path))
+    {
+        MDV_LOGE("DB storage directory creation failed: '%s'", path);
+        return 0;
+    }
+
+    #define MDV_DB_CALL(expr)                                                               \
+        if((rc = (expr)) != MDB_SUCCESS)                                                    \
+        {                                                                                   \
+            MDV_LOGE("The LMDB initialization failed: '%s' (%d)", mdb_strerror(rc), rc);    \
+            mdb_env_close(env);                                                             \
+            env = 0;                                                                        \
+            return 0;                                                                       \
+        }
+
+    int rc = 0;
+    MDB_env *env = 0;
+
+    MDV_DB_CALL(mdb_env_create(&env));
+    MDV_DB_CALL(mdb_env_set_maxdbs(env, dbs_num));
+    MDV_DB_CALL(mdb_env_open(env, path, flags, 0664));
+
+    #undef MDV_DB_CALL
+
+    mdv_storage *pstorage = (mdv_storage *)mdv_alloc(sizeof(mdv_storage));
+    if (!pstorage)
+    {
+        MDV_LOGE("Memory allocation failed");
+        mdb_env_close(env);
+    }
+
+    atomic_init(&pstorage->ref_counter, 1);
+    pstorage->env = env;
+
+    return pstorage;
+}
+
+
+mdv_storage * mdv_storage_retain(mdv_storage *pstorage)
+{
+    if (pstorage)
+        atomic_fetch_add_explicit(&pstorage->ref_counter, 1, memory_order_relaxed);
+    return pstorage;
+}
+
+
+void mdv_storage_release(mdv_storage *pstorage)
+{
+    if (pstorage)
+    {
+        if (atomic_fetch_sub_explicit(&pstorage->ref_counter, 1, memory_order_relaxed) == 1)
+        {
+            mdb_env_close(pstorage->env);
+            mdv_free(pstorage);
+        }
+    }
+}
+
+
+bool mdv_transaction_start(mdv_storage *pstorage, mdv_transaction *ptransaction)
+{
+    MDB_txn *txn;
+
+    int rc = mdb_txn_begin(pstorage->env, 0, 0, &txn);
+
+    if(rc != MDB_SUCCESS)
+    {
+        MDV_LOGE("The LMDB transaction wasn't started: '%s' (%d)", mdb_strerror(rc), rc);
+        return false;
+    }
+
+    ptransaction->pstorage = mdv_storage_retain(pstorage);
+    ptransaction->ptransaction = txn;
+
+    return true;
+}
+
+
+bool mdv_transaction_commit(mdv_transaction *ptransaction)
+{
+    MDB_txn *txn = (MDB_txn*)ptransaction->ptransaction;
+
+    if (txn)
+    {
+        int rc = mdb_txn_commit(txn);
+        if(rc != MDB_SUCCESS)
+            MDV_LOGE("The LMDB transaction wasn't committed: '%s' (%d)", mdb_strerror(rc), rc);
+
+        mdv_storage_release(ptransaction->pstorage);
+
+        ptransaction->ptransaction = 0;
+        ptransaction->pstorage = 0;
+
+        return rc == MDB_SUCCESS;
+    }
+
+    return false;
+}
+
+
+bool mdv_transaction_abort(mdv_transaction *ptransaction)
+{
+    MDB_txn *txn = (MDB_txn*)ptransaction->ptransaction;
+
+    if (txn)
+    {
+        mdb_txn_abort(txn);
+
+        mdv_storage_release(ptransaction->pstorage);
+
+        ptransaction->ptransaction = 0;
+        ptransaction->pstorage = 0;
+
+        return true;
+    }
+
+    return false;
+}
+
+
+bool mdv_map_open(mdv_transaction *ptransaction, char const *name, uint32_t flags, mdv_map *pmap)
+{
+    uint32_t mdb_flags = 0;
+    if (flags & MDV_MAP_CREATE)           mdb_flags |= MDB_CREATE;
+    if (flags & MDV_MAP_MULTI)            mdb_flags |= MDB_DUPSORT;
+    if (flags & MDV_MAP_INTEGERKEY)       mdb_flags |= MDB_INTEGERKEY;
+    if (flags & MDV_MAP_FIXED_SIZE_VALUE) mdb_flags |= MDB_DUPFIXED;
+    if (flags & MDV_MAP_INTEGERVAL)       mdb_flags |= MDB_INTEGERDUP;
+
+    MDB_txn *txn = (MDB_txn*)ptransaction->ptransaction;
+    MDB_dbi dbi;
+    int rc = mdb_dbi_open(txn, name, mdb_flags, &dbi);
+    if(rc != MDB_SUCCESS)
+    {
+        MDV_LOGE("Unable to open the LMDB database: '%s' (%d)", mdb_strerror(rc), rc);
+        return false;
+    }
+
+    pmap->pstorage = mdv_storage_retain(ptransaction->pstorage);
+    pmap->dbmap = dbi;
+
+    return true;
+}
+
+
+void mdv_map_close(mdv_map *pmap)
+{
+    if (pmap)
+    {
+        MDB_dbi dbi = (MDB_dbi)pmap->dbmap;
+        if (pmap->pstorage && dbi)
+        {
+            mdb_dbi_close(pmap->pstorage->env, dbi);
+            mdv_storage_release(pmap->pstorage);
+            pmap->pstorage = 0;
+        }
+    }
+}
 
 
--- mdv_core/storage/mdv_storage.h
+++ mdv_core/storage/mdv_storage.h
@@ -1,4 +1,50 @@
 #pragma once
+#include <stdint.h>
+#include <stdbool.h>
+
+
+typedef struct mdv_storage mdv_storage;
+
+
+mdv_storage * mdv_storage_open(char const *path, uint32_t dbs_num, uint32_t flags);
+mdv_storage * mdv_storage_retain(mdv_storage *pstorage);
+void          mdv_storage_release(mdv_storage *pstorage);
+
+
+typedef struct
+{
+    mdv_storage *pstorage;
+    void        *ptransaction;
+} mdv_transaction;
+
+
+bool mdv_transaction_start(mdv_storage *pstorage, mdv_transaction *ptransaction);
+bool mdv_transaction_commit(mdv_transaction *ptransaction);
+bool mdv_transaction_abort(mdv_transaction *ptransaction);
+
+
+typedef struct
+{
+    mdv_storage *pstorage;
+    unsigned int dbmap;
+} mdv_map;
+
+
+enum mdv_map_flags
+{
+    MDV_MAP_CREATE              = 1 << 0,   // Create the map if it doesn't exist
+    MDV_MAP_MULTI               = 1 << 1,   // Duplicate keys may be used in the map
+    MDV_MAP_INTEGERKEY          = 1 << 2,   // Keys are binary integers in native byte order
+    MDV_MAP_FIXED_SIZE_VALUE    = 1 << 3,   // The data items for this map are all the same size. (used only with DB_MAP_MULTI)
+    MDV_MAP_INTEGERVAL          = 1 << 4    // This option specifies that duplicate data items are binary integers
+};
+
+
+bool mdv_map_open(mdv_transaction *ptransaction, char const *name, uint32_t flags, mdv_map *pmap);
+void mdv_map_close(mdv_map *pmap);
+
+
+
 
 
 typedef struct

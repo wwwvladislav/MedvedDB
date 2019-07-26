4119114adad4569418487739b8ed3c02b1c68707
Vladislav Volkov <wwwvladislav@gmail.com>
2019-07-27 01:38:29 +0300

    Documentation for UUID

--- mdv_platform/mdv_uuid.h
+++ mdv_platform/mdv_uuid.h
@@ -1,14 +1,20 @@
+/**
+ * @file
+ * @brief Universally Unique IDentifier
+ */
+
 #pragma once
 #include "mdv_string.h"
 #include "mdv_def.h"
 
 
+/// Universally Unique IDentifier
 typedef struct
 {
     union
     {
-        uint8_t  u8[16];
-        uint64_t u64[2];
+        uint8_t  u8[16];    ///< UUID representation as u8 array
+        uint64_t u64[2];    ///< UUID representation as u64 array
 
         struct
         {
@@ -24,8 +30,54 @@ typedef struct
 } mdv_uuid;
 
 
-mdv_uuid    mdv_uuid_generate();
-int         mdv_uuid_cmp(mdv_uuid const *a, mdv_uuid const *b);
-size_t      mdv_uuid_hash(mdv_uuid const *a);
-mdv_string  mdv_uuid_to_str(mdv_uuid const *uuid);
-bool        mdv_uuid_from_str(mdv_uuid *uuid, mdv_string const *str);
+/**
+ * @brief New UUID generation
+ *
+ * @return New randomly generated UUID
+ */
+mdv_uuid mdv_uuid_generate();
+
+
+/**
+ * @brief Two UUIDs comparision
+ *
+ * @param a [in]    first UUID
+ * @param b [in]    second UUID
+ *
+ * @return an integer less than zero if a is less then b
+ * @return zero if a is equal to b
+ * @return an integer greater than zero if a is greater then b
+ */
+int mdv_uuid_cmp(mdv_uuid const *a, mdv_uuid const *b);
+
+
+/**
+ * @brief Hash function for UUIDs
+ *
+ * @param a [in]    UUID
+ *
+ * @return hash value for UUID
+ */
+size_t mdv_uuid_hash(mdv_uuid const *a);
+
+
+/**
+ * @brief Convert UUID to string
+ *
+ * @param uuid [in] UUID
+ *
+ * @return string representation of UUID
+ */
+mdv_string mdv_uuid_to_str(mdv_uuid const *uuid);
+
+
+/**
+ * @brief Convert string to UUID
+ *
+ * @param uuid [out]    UUID
+ * @param str [in]      string representation of UUID
+ *
+ * @return On success, return true and UUID is placed to UUID pointer
+ * @return On error, return false
+ */
+bool mdv_uuid_from_str(mdv_uuid *uuid, mdv_string const *str);

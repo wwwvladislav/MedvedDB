99b1c988f7475b7a3d1ff2d719fa5a5156b1a822
vlad <vlad@wsmnn-265.netcracker.com>
2019-07-29 15:26:43 +0300

    Gossip algorithm

--- mdv_platform/mdv_bloom.h
+++ mdv_platform/mdv_bloom.h
@@ -1,6 +1,12 @@
 /**
- * @file
+ * @file mdv_bloom.h
+ * @author Vladislav Volkov (wwwvladislav@gmail.com)
  * @brief Bloom filter
+ * @version 0.1
+ * @date 2019-07-29
+ * 
+ * @copyright Copyright (c) 2019
+ * 
  */
 #pragma once
 #include <math.h>
---
+++ mdv_platform/mdv_gossip.h
@@ -0,0 +1,11 @@
+/**
+ * @file mdv_gossip.h
+ * @author Vladislav Volkov (wwwvladislav@gmail.com)
+ * @brief Gossip protocol implementation for message broadcasting.
+ * @version 0.1
+ * @date 2019-07-29
+ * 
+ * @copyright Copyright (c) 2019, Vladislav Volkov
+ * 
+ */
+#pragma once

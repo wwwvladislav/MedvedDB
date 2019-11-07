41d1d28262dfc4eb722dd631869b721640246719
Vladislav Volkov <wwwVladislav@gmail.com>
2019-11-07 19:20:43 +0300

    Topology tracking in connection manager

--- mdv_core/mdv_conman.c
+++ mdv_core/mdv_conman.c
@@ -2,12 +2,15 @@
 #include "mdv_user.h"
 #include "mdv_peer.h"
 #include "mdv_conctx.h"
+#include "event/mdv_types.h"
+#include "event/mdv_topology.h"
 #include <mdv_chaman.h>
 #include <mdv_log.h>
 #include <mdv_alloc.h>
 #include <mdv_socket.h>
 #include <mdv_ctypes.h>
 #include <mdv_rollbacker.h>
+#include <mdv_safeptr.h>
 
 
 /// Connections manager
@@ -16,6 +19,7 @@ struct mdv_conman
     mdv_uuid         uuid;          ///< Current cluster node UUID
     mdv_chaman      *chaman;        ///< Channels manager
     mdv_ebus        *ebus;          ///< Events bus
+    mdv_safeptr     *topology;      ///< Current topology
 };
 
 
@@ -81,8 +85,12 @@ static void * mdv_conman_ctx_create(mdv_descriptor    fd,
     switch(type)
     {
         case MDV_CTX_USER:
-            conctx = mdv_user_create(fd, conman->ebus, &mdv_empty_topology);
+        {
+            mdv_topology *topology = mdv_safeptr_get(conman->topology);
+            conctx = mdv_user_create(fd, conman->ebus, topology);
+            mdv_topology_release(topology);
             break;
+        }
 
         case MDV_CTX_PEER:
             conctx = mdv_peer_create(&conman->uuid, dir, fd, conman->ebus);
@@ -161,9 +169,24 @@ static void mdv_conman_ctx_closed(void *userdata, void *ctx)
 }
 
 
+static mdv_errno mdv_conman_evt_topology(void *arg, mdv_event *event)
+{
+    mdv_conman *conman = arg;
+    mdv_evt_topology *topo = (mdv_evt_topology *)event;
+    return MDV_OK;
+    //return mdv_safeptr_set(conman->topology, topo->topology);
+}
+
+
+static const mdv_event_handler_type mdv_conman_handlers[] =
+{
+    { MDV_EVT_TOPOLOGY,    mdv_conman_evt_topology },
+};
+
+
 mdv_conman * mdv_conman_create(mdv_conman_config const *conman_config, mdv_ebus *ebus)
 {
-    mdv_rollbacker *rollbacker = mdv_rollbacker_create(2);
+    mdv_rollbacker *rollbacker = mdv_rollbacker_create(4);
 
     mdv_conman *conman = mdv_alloc(sizeof(mdv_conman), "conman");
 
@@ -210,9 +233,36 @@ mdv_conman * mdv_conman_create(mdv_conman_config const *conman_config, mdv_ebus 
         return 0;
     }
 
-    mdv_rollbacker_free(rollbacker);
+    mdv_rollbacker_push(rollbacker, mdv_chaman_free, conman->chaman);
 
     conman->ebus = mdv_ebus_retain(ebus);
+
+    mdv_rollbacker_push(rollbacker, mdv_ebus_release, ebus);
+
+    conman->topology = mdv_safeptr_create(&mdv_empty_topology,
+                                          (mdv_safeptr_retain_fn)mdv_topology_retain,
+                                          (mdv_safeptr_release_fn)mdv_evt_topology_release);
+
+    if (!conman->topology)
+    {
+        MDV_LOGE("Safe pointer creation failed");
+        mdv_rollback(rollbacker);
+        return 0;
+    }
+
+    mdv_rollbacker_push(rollbacker, mdv_safeptr_free, conman->topology);
+
+    if (mdv_ebus_subscribe_all(conman->ebus,
+                               conman,
+                               mdv_conman_handlers,
+                               sizeof mdv_conman_handlers / sizeof *mdv_conman_handlers) != MDV_OK)
+    {
+        MDV_LOGE("Ebus subscription failed");
+        mdv_rollback(rollbacker);
+        return 0;
+    }
+
+    mdv_rollbacker_free(rollbacker);
 
     return conman;
 }
@@ -223,7 +273,15 @@ void mdv_conman_free(mdv_conman *conman)
     if(conman)
     {
         mdv_chaman_free(conman->chaman);
+
+        mdv_ebus_unsubscribe_all(conman->ebus,
+                                 conman,
+                                 mdv_conman_handlers,
+                                 sizeof mdv_conman_handlers / sizeof *mdv_conman_handlers);
+
+        mdv_safeptr_free(conman->topology);
         mdv_ebus_release(conman->ebus);
+
         mdv_free(conman, "conman");
     }
 }

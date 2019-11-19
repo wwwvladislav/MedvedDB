e70e8c869612dc82cc75c724e4ee42aaca7eb13f
Vladislav Volkov <wwwvladislav@gmail.com>
2019-11-19 11:56:52 +0300

    Row set module

---
+++ mdv_types/mdv_rowset.h
@@ -0,0 +1,11 @@
+/**
+ * @file mdv_rowset.h
+ * @author Vladislav Volkov (wwwvladislav@gmail.com)
+ * @brief Interface and useful types definitions for row set access.
+ * @version 0.1
+ * @date 2019-11-19
+ *
+ * @copyright Copyright (c) 2019
+ *
+ */
+#pragma once
